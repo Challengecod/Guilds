d0dd4089c4f7d1c4a8f69847fea4b2a46010e023
Challengecod <145475982+Challengecod@users.noreply.github.com>
2024-05-06 10:56:04 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print('There is no tommorow')
+import pygame
+
+# set up pygame modules
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 15)
+pygame.display.set_caption("")
+
+# set up variables for the display
+SCREEN_HEIGHT = 780
+SCREEN_WIDTH = 1300
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(size)
+
+r = 255
+g = 255
+b = 255
+
+# The loop will carry on until the user exits the game (e.g. clicks the close button).
+run = True
+
+
+
+# -------- Main Program Loop -----------
+while run:
+
+    # --- Main event loop
+    ## ----- NO BLIT ZONE START ----- ##
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+
+    ##  ----- NO BLIT ZONE END  ----- ##
+
+    ## FILL SCREEN, and BLIT here ##
+    screen.fill((r, g, b))
+
+
+
+    pygame.display.update()
+    ## END OF WHILE LOOP
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
